474b11b5ea13db7f5da37f99219d22b2d3519971
robintranberg <78171917+robintranberg@users.noreply.github.com>
2024-11-25 20:59:59 +0000

    Adding homepage

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+#Importing flask, render_template to render pages.
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "Hello Robinho"
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
